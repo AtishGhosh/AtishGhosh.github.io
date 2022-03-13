c15acd45fe4a3018432524a6d20d5210237906c0
AtishGhosh <atishghosh30@gmail.com>
2022-03-13 18:02:33 +0530

    LT v0.1

--- README.md
+++
@@ -1,1 +0,0 @@
-# AtishGhosh.github.io
--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let header = this.document.querySelector('nav');
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('scroll-active', windowPosition);
-});
